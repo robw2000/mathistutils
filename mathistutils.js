625d01b369591e1525dcb462b012b5c6bd8c1dc0
Rob Wilburn <rob@mathist.net>
2013-07-29 12:22:12 -0600

    Added array default value to range_defval and added tests

--- mathistutils.js
+++ mathistutils.js
@@ -2,17 +2,17 @@ var util = require('util')
   , underscore = require('underscore');
 
 // Extend underscore
-var MathistUtils = module.exports = underscore;
+var mu = module.exports = underscore;
 
-MathistUtils.ulamConjCount = function(num) {var count = 0; while (num > 1) {num = (num % 2 == 0) ? num / 2 : 3 * num + 1; count++;}; return count;}
-MathistUtils.TestObj = function(index) {this.index = index; this.ulam = MathistUtils.ulamConjCount(index);}
-MathistUtils.range_defval = function(s, e, v) {var d = (v !== undefined); var r = []; for (var i = s; i < e; i++) {r.push(d?v:i)}; return r;}
-MathistUtils.copyArray = function(a) {return a.map(function(x){return x;});}
-MathistUtils.numsort = function(l) {l.sort(function(a,b){return a-b})}
+mu.ulamConjCount = function(num) {var count = 0; while (num > 1) {num = (num % 2 == 0) ? num / 2 : 3 * num + 1; count++;}; return count;}
+mu.TestObj = function(index) {this.index = index; this.ulam = mu.ulamConjCount(index);}
+mu.copyArray = function(a) {var b = []; a.forEach(function(x){b.push(x);}); return b;}
+mu.range_defval = function(s, e, v) {var d = (v !== undefined); if (mu.isArray(v)) {v = mu.copyArray(v)}; var r = []; for (var i = s; i < e; i++) {r.push(d?mu.isArray(v)?mu.copyArray(v):v:i)}; return r;}
+mu.numsort = function(l) {l.sort(function(a,b){return a-b})}
 
 // Sort an array of numeric arrays
 // Example: [[0, 3, 5], [2, 5, 8], [0, 1, 2]] => [[0, 1, 2], [0, 3, 5], [2, 5, 8]]
-MathistUtils.sortnumarrays = function(l, sortDesc, internalSortDesc) {
+mu.sortnumarrays = function(l, sortDesc, internalSortDesc) {
   var mod = (sortDesc) ? -1 : 1;
   var modInt = (internalSortDesc) ? -1 : 1;
   l.forEach(function(sub) {sub.sort(function(a, b) { return modInt * (a - b); });});
@@ -28,10 +28,10 @@ MathistUtils.sortnumarrays = function(l, sortDesc, internalSortDesc) {
 };
 
 // This doesn't work with objects
-MathistUtils.sameContents = function(l1, l2) {
+mu.sameContents = function(l1, l2) {
   if (l1.length !== l2.length) return false;
-  var a1 = MathistUtils.copyArray(l1).sort();
-  var a2 = MathistUtils.copyArray(l2).sort();
+  var a1 = mu.copyArray(l1).sort();
+  var a2 = mu.copyArray(l2).sort();
   for (var i = 0; i < a1.length; i++) {
     if (a1[i] !== a2[i]) return false;
   }
@@ -39,15 +39,15 @@ MathistUtils.sameContents = function(l1, l2) {
 }
 
 // Grab all the util functions
-MathistUtils.format = util.format;
-MathistUtils.debug = util.debug;
-MathistUtils.error = util.error;
-MathistUtils.puts = util.puts;
-MathistUtils.print = util.print;
-MathistUtils.log = util.log;
-MathistUtils.inspect = util.inspect;
-MathistUtils.isArray = util.isArray;
-MathistUtils.isRegExp = util.isRegExp;
-MathistUtils.isDate = util.isDate;
-MathistUtils.isError = util.isError;
-MathistUtils.inherits = util.inherits;
+mu.format = util.format;
+mu.debug = util.debug;
+mu.error = util.error;
+mu.puts = util.puts;
+mu.print = util.print;
+mu.log = util.log;
+mu.inspect = util.inspect;
+mu.isArray = util.isArray;
+mu.isRegExp = util.isRegExp;
+mu.isDate = util.isDate;
+mu.isError = util.isError;
+mu.inherits = util.inherits;
--- test/functiontests.js
+++ test/functiontests.js
@@ -31,11 +31,80 @@ describe('MathistUtils.TestObject tests', function () {
 describe('MathistUtils.range_defval tests', function () {
 
   it ('should make an array of 5 values, each set to null', function (done) {
-    var result = utils.range_defval(0, 5, null);
+    var defval = null;
+    var result = utils.range_defval(0, 5, defval);
     assert.equal(5, result.length);
     for (var i = 0; i < 5; i++) {
-      assert.equal(null, result[i]);
+      assert.equal(defval, result[i]);
     }
     done();
   });
+
+  it ('should make an array of 5 values, each set to -1', function (done) {
+    var defval = -1;
+    var result = utils.range_defval(0, 5, defval);
+    assert.equal(5, result.length);
+    for (var i = 0; i < 5; i++) {
+      assert.equal(defval, result[i]);
+    }
+    done();
+  });
+
+  it ('should make an array of 5 values, each set to 0', function (done) {
+    var defval = 0;
+    var result = utils.range_defval(0, 5, defval);
+    assert.equal(5, result.length);
+    for (var i = 0; i < 5; i++) {
+      assert.equal(defval, result[i]);
+    }
+    done();
+  });
+
+  it ('should make an array of 5 values, each set to false', function (done) {
+    var defval = false;
+    var result = utils.range_defval(0, 5, defval);
+    assert.equal(5, result.length);
+    for (var i = 0; i < 5; i++) {
+      assert.equal(defval, result[i]);
+    }
+    done();
+  });
+
+  it ('should make an array of 5 values, each set to true', function (done) {
+    var defval = true;
+    var result = utils.range_defval(0, 5, defval);
+    assert.equal(5, result.length);
+    for (var i = 0; i < 5; i++) {
+      assert.equal(defval, result[i]);
+    }
+    done();
+  });
+
+  it ('should make an array of 5 values, each set to []', function (done) {
+    var defval = [];
+    var result = utils.range_defval(0, 5, defval);
+    assert.equal(5, result.length);
+    for (var i = 0; i < 5; i++) {
+      assert(utils.isArray(result[i]));
+      assert.equal(0, result[i].length);
+    }
+    done();
+  });
+
+  it ('should make an array of 5 values, each set to [0]', function (done) {
+    var defval = [0];
+    var result = utils.range_defval(0, 5, defval);
+    assert.equal(5, result.length);
+
+    defval[0] = -1;
+    for (var i = 0; i < 5; i++) {
+      assert(utils.isArray(result[i]));
+      assert.equal(1, result[i].length);
+      assert.equal(0, result[i][0]);
+      result[i][0] = i + 10;
+    }
+    assert.equal(10, result[0][0]);
+    done();
+  });
+
 });
